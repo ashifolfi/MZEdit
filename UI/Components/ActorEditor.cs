91eaac6303ffe6b9de35efdb1593ade3dc4d2523
K J <anticream39@yahoo.com>
2024-10-11 14:32:06 -0500

    add basic actor editing

--- UI/Components/ActorEditor.cs
+++ UI/Components/ActorEditor.cs
@@ -1,11 +1,23 @@
 using Godot;
+using MZEdit.Data;
 using System;
 
 namespace MZEdit.UI.Components;
 
 public partial class ActorEditor : Control
 {
-    [Export] public int ActorID { get; set; }
+    public int ActorID
+    {
+        get => m_Actor.Id;
+        set
+        {
+            m_Actor = EditorMain.Instance.Actors.Find((e) => e != null && e.Id == value);
+            RefreshInterface();
+        }
+    }
+    private MVActor m_Actor;
+
+    public Action ActorEdited;
 
     [Export] public TextureRect FieldFace;
     [Export] public TextureRect FieldCharacter;
@@ -17,4 +29,68 @@ public partial class ActorEditor : Control
     [Export] public SpinBox FieldMaxLevel;
     [Export] public TextEdit FieldProfile;
     [Export] public TextEdit FieldNote;
+    
+    public void RefreshInterface()
+    {
+        FieldName.Text = m_Actor.Name;
+        FieldNickName.Text = m_Actor.Nickname;
+
+
+        FieldClass.Clear();
+        foreach (var classInf in EditorMain.Instance.Classes)
+        {
+            if (classInf != null)
+            {
+                FieldClass.AddItem(classInf.Name, classInf.Id);
+            }
+        }
+        FieldClass.Select(m_Actor.ClassId - 1);
+
+        FieldInitialLevel.Value = m_Actor.InitialLevel;
+        FieldMaxLevel.Value = m_Actor.MaxLevel;
+        FieldProfile.Text = m_Actor.Profile;
+        FieldNote.Text = m_Actor.Note;
+    }
+
+    private void OnInitialLevelChanged(float value)
+    {
+        m_Actor.InitialLevel = (int)value;
+        ActorEdited?.Invoke();
+    }
+
+    private void OnMaxLevelChanged(float value)
+    {
+        m_Actor.MaxLevel = (int)value;
+        ActorEdited?.Invoke();
+    }
+
+    private void OnClassChanged(int index)
+    {
+        m_Actor.ClassId = FieldClass.GetItemId(index);
+        ActorEdited?.Invoke();
+    }
+
+    private void OnNameChanged(string newText)
+    {
+        m_Actor.Name = newText;
+        ActorEdited?.Invoke();
+    }
+
+    private void OnNicknameChanged(string newText)
+    {
+        m_Actor.Nickname = newText;
+        ActorEdited?.Invoke();
+    }
+
+    private void OnProfileChanged()
+    {
+        m_Actor.Profile = FieldProfile.Text;
+        ActorEdited?.Invoke();
+    }
+
+    private void OnNoteChanged()
+    {
+        m_Actor.Note = FieldNote.Text;
+        ActorEdited?.Invoke();
+    }
 }
--- UI/DatabaseEditor.cs
+++ UI/DatabaseEditor.cs
@@ -1,6 +1,8 @@
 using Godot;
 using System;
 using log4net;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MZEdit.UI;
 
@@ -8,11 +10,74 @@ public partial class DatabaseEditor : Window
 {
     private static readonly ILog Log = LogManager.GetLogger("DatabaseEditor");
 
+	[ExportGroup("SubControls (PRIVATE)")]
+	[Export] private ItemList ActorList;
+	[Export] private Components.ActorEditor ActorEditor;
+
+    [Export] private ItemList ClassList;
+    [Export] private ItemList ItemList;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
+		EditorMain.Instance.OnProjectLoaded += OnProjectLoad;
 		Hide();
 	}
+
+	private void OnActorChanged()
+	{
+		var actor = EditorMain.Instance.Actors.Find(e => e != null && e.Id == ActorEditor.ActorID);
+		ActorList.SetItemText(actor.Id - 1, $"{actor.Id}: {actor.Name}");
+	}
+
+	private void OnProjectLoad()
+	{
+		Log.Info("Refreshing Database contents");
+		ActorList.Clear();
+		ClassList.Clear();
+		ItemList.Clear();
+
+		{ // Actors
+			var sortedList = EditorMain.Instance.Actors
+				.Where((e) => e != null)
+				.ToList();
+			sortedList.Sort((a, b) => a.Id.CompareTo(b.Id));
+
+			foreach (var item in sortedList)
+			{
+				ActorList.AddItem($"{item.Id}: {item.Name}");
+			}
+		}
+
+        { // Classes
+            var sortedList = EditorMain.Instance.Classes
+                .Where((e) => e != null)
+                .ToList();
+            sortedList.Sort((a, b) => a.Id.CompareTo(b.Id));
+
+            foreach (var item in sortedList)
+            {
+                ClassList.AddItem($"{item.Id}: {item.Name}");
+            }
+        }
+
+        { // Items
+            var sortedList = EditorMain.Instance.Items
+                .Where((e) => e != null)
+                .ToList();
+            sortedList.Sort((a, b) => a.Id.CompareTo(b.Id));
+
+            foreach (var item in sortedList)
+            {
+                ItemList.AddItem($"{item.Id}: {item.Name}");
+            }
+        }
+    }
+
+	private void OnActorSelected(int item)
+	{
+		ActorEditor.ActorID = item + 1;
+    }
 
 	private void OnClose()
 	{
