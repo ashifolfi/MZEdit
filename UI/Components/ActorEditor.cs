e2fdf80b0399f250d2259eb2db42952e0481ff33
K J <anticream39@yahoo.com>
2024-10-10 23:17:47 -0500

    start adding logic to actor editor

---
+++ UI/Components/ActorEditor.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class ActorEditor : Control
+{
+    [Export] public int ActorID { get; set; }
+
+    [Export] public TextureRect FieldFace;
+    [Export] public TextureRect FieldCharacter;
+    [Export] public TextureRect FieldBattler;
+    [Export] public LineEdit FieldName;
+    [Export] public LineEdit FieldNickName;
+    [Export] public OptionButton FieldClass;
+    [Export] public SpinBox FieldInitialLevel;
+    [Export] public SpinBox FieldMaxLevel;
+    [Export] public TextEdit FieldProfile;
+    [Export] public TextEdit FieldNote;
+}
