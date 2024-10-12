c1b53e314c55b51d3cd31d965f5a7f4eaedc4290
K J <anticream39@yahoo.com>
2024-10-12 13:28:07 -0500

    WIP: Troop data

---
+++ Data/MVTroop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace MZEdit.Data;
+
+public class MVTroop
+{
+    public struct STroopMember
+    {
+        [JsonProperty("enemyId")] public int EnemyId;
+        [JsonProperty("x")] public int X;
+        [JsonProperty("y")] public int Y;
+        [JsonProperty("hidden")] public bool Hidden;
+    }
+
+    [JsonProperty("id")] public int Id;
+    [JsonProperty("name")] public string Name;
+    [JsonProperty("members")] public List<STroopMember> Members;
+    // todo: Pages element
+}
